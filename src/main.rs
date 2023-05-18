af2e3157ed863612166ecb995de2b75e072c3fe8
David Antliff <david@pitchblende.co.nz>
2023-05-18 12:08:00 +1200

    Run as axum web server.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
 // https://fasterthanli.me/series/building-a-rust-service-with-nix/part-3
 
-use pretty_hex::PrettyHex;
+use axum::{
+    body::BoxBody,
+    http::header,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router, ServiceExt,
+};
+use reqwest::StatusCode;
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    // let url = get_cat_image_url().await.unwrap();
-    // println!("The image is at {}", url);
-    // let image_bytes = get_cat_image_bytes().await.unwrap();
-    // println!("{:?}", &image_bytes[..200].hex_dump());
-    let art = get_cat_ascii_art().await.unwrap();
-    println!("{art}");
+    let app = Router::new().route("/", get(root_get));
+
+    axum::Server::bind(&"0.0.0.0:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root_get() -> Response<BoxBody> {
+    match get_cat_ascii_art().await {
+        Ok(art) => (
+            StatusCode::OK,
+            [(header::CONTENT_TYPE, "text/plain; charset=utf-8")],
+            art,
+        )
+            .into_response(),
+        Err(e) => {
+            println!("Something went wrong: {e}");
+            (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
+        }
+    }
 }
 
 async fn get_cat_ascii_art() -> color_eyre::Result<String> {
@@ -41,61 +63,12 @@ async fn get_cat_ascii_art() -> color_eyre::Result<String> {
         .await?;
 
     let image = image::load_from_memory(&image_bytes)?;
-    let ascii_art = artem::convert(image, artem::options::OptionBuilder::new().build());
+    let ascii_art = artem::convert(
+        image,
+        artem::options::OptionBuilder::new()
+            //.target(artem::options::TargetType::HtmlFile(true, true))
+            .build(),
+    );
 
     Ok(ascii_art)
-}
-
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
-    #[derive(Deserialize)]
-    struct CatImage {
-        url: String,
-    }
-
-    let api_url = "https://api.thecatapi.com/v1/images/search";
-    let client = reqwest::Client::default();
-
-    let image = client
-        .get(api_url)
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<Vec<CatImage>>()
-        .await?
-        .pop()
-        .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
-
-    Ok(client
-        .get(image.url)
-        .send()
-        .await?
-        .error_for_status()?
-        .bytes()
-        .await?
-        .to_vec())
-}
-
-async fn get_cat_image_url() -> color_eyre::Result<String> {
-    let api_url = "https://api.thecatapi.com/v1/images/search";
-    let res = reqwest::get(api_url).await?;
-    if !res.status().is_success() {
-        return Err(color_eyre::eyre::eyre!(
-            "The Cat API returned HTTP {}",
-            res.status(),
-        ));
-    }
-
-    #[derive(Deserialize)]
-    struct CatImage {
-        //id: String,
-        url: String,
-        // width: usize,
-        // height: usize,
-    }
-
-    let mut images: Vec<CatImage> = res.json().await?;
-    let Some(image) = images.pop() else {
-        return Err(color_eyre::eyre::eyre!("The Cat API returned no images"))
-    };
-    Ok(image.url)
 }
